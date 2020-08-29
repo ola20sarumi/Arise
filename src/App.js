ec8cc7cf3183fab96e5ca9677b1f7601af3358e7
ola20sarumi <ola20sarumi@hotmail.com>
2020-08-29 12:53:41 +0300

    components and images updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import ContactUs from "./components/pages/ContactUs";
+import SignUp from "./components/pages/SignUp";
+import Marketing from "./components/pages/Marketing";
+import Consulting from "./components/pages/Consulting";
+import Design from "./components/pages/Design";
+import Development from "./components/pages/Development";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/contact-us" exact component={ContactUs} />
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/marketing" exact component={Marketing} />
+        <Route path="/consulting" exact component={Consulting} />
+        <Route path="/design" exact component={Design} />
+        <Route path="/development" exact component={Development} />
+      </Switch>
     </Router>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,27 @@ function Navbar() {
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="navbar-logo">
-          ARISE
+          ARISE <i class="fas fa-truck-monster" />
         </Link>
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
@@ -25,7 +41,11 @@ function Navbar() {
               Home
             </Link>
           </li>
-          <li className="nav-item">
+          <li
+            className="nav-item"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
             <Link
               to="/services"
               className="nav-links"
@@ -34,6 +54,15 @@ function Navbar() {
               Services <i className="fas fa-caret-down" />
             </Link>
             {dropdown && <Dropdown />}
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/products"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Products
+            </Link>
           </li>
           <li className="nav-item">
             <Link
